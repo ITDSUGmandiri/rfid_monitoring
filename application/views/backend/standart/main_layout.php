a9858f9a5edc528816be8bda44d7f2c8f190da08
becauseoffyou <irhandy96@gmail.com>
2025-01-15 12:57:36 +0700

    fixing load library

--- application/views/backend/standart/main_layout.php
+++ application/views/backend/standart/main_layout.php
@@ -265,11 +265,12 @@
 
 
 
-  <script src="https://cdn.datatables.net/2.2.0/js/dataTables.js">
-  </script>
+
 
   <?php if (basename(dirname($_SERVER['REQUEST_URI'])) != 'registrasi_aset') { ?>
     <script src="https://cdn.datatables.net/2.1.8/js/dataTables.js">
+    </script>
+    <script src="https://cdn.datatables.net/2.2.0/js/dataTables.js">
     </script>
   <?php } ?>
 
