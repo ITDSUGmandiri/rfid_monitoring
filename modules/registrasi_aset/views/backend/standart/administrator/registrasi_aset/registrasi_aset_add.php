554413cc8861441ff89fa7428eb5f06ebf1daf37
William Tansil <itbs.ugmandiri@gmail.com>
2025-01-15 11:27:58 +0700

    Perbaikan Library dan Data Table Registrasi Aset

--- modules/registrasi_aset/views/backend/standart/administrator/registrasi_aset/registrasi_aset_add.php
+++ modules/registrasi_aset/views/backend/standart/administrator/registrasi_aset/registrasi_aset_add.php
@@ -518,8 +518,8 @@
 
 <!-- 
 <script src="https://code.jquery.com/jquery-3.3.1.js"></script> -->
-<script src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>
-<script src="https://cdn.datatables.net/select/1.2.7/js/dataTables.select.min.js"></script>
+<!-- <script src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>
+<script src="https://cdn.datatables.net/select/1.2.7/js/dataTables.select.min.js"></script> -->
 <script src="<?php echo base_url(); ?>asset/js/socket.io.js"></script>
 
 <script type="text/javascript">
