2f970484ebd7cf99b44cfc10c69b52f753ef4c43
becauseoffyou <irhandy96@gmail.com>
2025-01-17 16:18:49 +0700

    fixing pilihan ruangan

--- modules/tag_reader/views/backend/standart/administrator/tag_reader/tag_reader_add.php
+++ modules/tag_reader/views/backend/standart/administrator/tag_reader/tag_reader_add.php
@@ -46,7 +46,7 @@
                                     ?>
 
                                     <?php foreach (db_get_all_data('tb_master_ruangan', $conditions) as $row): ?>
-                                        <option value="<?= $row->id_room ?>"><?= $row->name_room; ?></option>
+                                        <option value="<?= $row->id ?>"><?= $row->ruangan; ?></option>
                                     <?php endforeach; ?>
                                 </select>
                                 <small class="info help-block">
