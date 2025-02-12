4c6d54ae5aee3a36d8971067ce5c1a201a9b8d6e
becauseoffyou <irhandy96@gmail.com>
2025-02-12 19:27:32 +0700

    tambah tanggal dan kembali peminjaman

--- modules/dashboard/dashboard/controllers/backend/Dashboard.php
+++ modules/dashboard/dashboard/controllers/backend/Dashboard.php
@@ -65,7 +65,7 @@ class Dashboard extends Admin
 				$data_json = $this->db->query($query_anomali)->result();
 				break;
 			case "mutation":
-				$query_mut = "SELECT x.kode_tid, x.nama_aset, x.kode_aset, x.nup, x.status, x.id_lokasi, y.id, y.ruangan FROM tb_master_aset x JOIN tb_master_ruangan y ON y.id = x.id_lokasi WHERE x.status = 2 AND x.kode_tid !=''";
+				$query_mut = "SELECT x.kode_tid, x.nama_aset, x.kode_aset, x.nup, x.status, x.id_lokasi, DATE_FORMAT(x.tgl_peminjaman, '%d/%m/%Y') as pinjam, DATE_FORMAT(x.tgl_pengembalian, '%d/%m/%Y') as kembali, y.id, y.ruangan FROM tb_master_aset x JOIN tb_master_ruangan y ON y.id = x.id_lokasi WHERE x.status = 2 AND x.kode_tid !=''";
 				$data_json = $this->db->query($query_mut)->result();
 				break;
 			case "moving":
