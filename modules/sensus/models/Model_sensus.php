885025f46aebc8a9e5dbc59915419671d3c15233
sm4rtschool <sm4rtschool@gmail.com>
2025-02-17 14:28:24 +0700

    fixing minor bugs di registrasi aset dan sensus

--- modules/sensus/models/Model_sensus.php
+++ modules/sensus/models/Model_sensus.php
@@ -62,41 +62,77 @@ class Model_sensus extends MY_Model
     {
         $iterasi = 1;
         $num = count($this->field_search);
-        $where = NULL;
+        // $where = NULL;
+        $where = "tb_master_transaksi.tipe_transaksi = 3";
         $q = $this->scurity($q);
         $field = $this->scurity($field);
         $field = in_array($field, $this->field_search) ? $field : "";
 
-        if (empty($field)) {
+        if ($field === "status_transaksi") {
+            switch (strtolower($q)) {
+                case "open":
+                    $where .= " AND tb_master_transaksi.status_transaksi = 1";
+                    break;
+                case "progress":
+                    $where .= " AND tb_master_transaksi.status_transaksi = 2";
+                    break;
+                case "complete":
+                    $where .= " AND tb_master_transaksi.status_transaksi = 3";
+                    break;
+                case "batal":
+                    $where .= " AND tb_master_transaksi.status_transaksi = 4";
+                    break;
+            }
+        } 
+        elseif (empty($field)) {
+            $where_conditions = [];
             foreach ($this->field_search as $field) {
                 $f_search = "tb_master_transaksi." . $field;
                 if (strpos($field, '.')) {
                     $f_search = $field;
                 }
-
-                if ($iterasi == 1) {
-                    $where .= $f_search . " LIKE '%" . $q . "%' ";
-                } else {
-                    $where .= "OR " . $f_search . " LIKE '%" . $q . "%' ";
-                }
-                $iterasi++;
+                $where_conditions[] = $f_search . " LIKE '%" . $q . "%'";
             }
-
-            $where = '(' . $where . ')';
+            if (!empty($where_conditions)) {
+                $where .= " AND (" . implode(" OR ", $where_conditions) . ")";
+            }
         } else {
-            $where .= "(" . "tb_master_transaksi." . $field . " LIKE '%" . $q . "%' )";
+            $where .= " AND (tb_master_transaksi." . $field . " LIKE '%" . $q . "%')";
         }
+
+        // if (empty($field)) {
+
+        //     foreach ($this->field_search as $field) {
+        //         $f_search = "tb_master_transaksi." . $field;
+        //         if (strpos($field, '.')) {
+        //             $f_search = $field;
+        //         }
+
+        //         if ($iterasi == 1) {
+        //             $where .= $f_search . " LIKE '%" . $q . "%' ";
+        //         } else {
+        //             $where .= "OR " . $f_search . " LIKE '%" . $q . "%' ";
+        //         }
+        //         $iterasi++;
+        //     }
+
+        //     $where = '(' . $where . ')';
+        // } else {
+        //     $where .= "(" . "tb_master_transaksi." . $field . " LIKE '%" . $q . "%' )";
+        // }
 
         if (is_array($select_field) and count($select_field)) {
             $this->db->select($select_field);
         }
 
         $this->join_avaiable()->filter_avaiable();
-        $this->db->where('tb_master_transaksi.tipe_transaksi = 3');
+        // $this->db->where('tb_master_transaksi.tipe_transaksi = 3');
+        $this->db->where($where, NULL, FALSE);
         $this->db->order_by('id', 'DESC');
         $this->db->limit($limit, $offset);
         $this->sortable();
         $query = $this->db->get($this->table_name);
+        // echo $this->db->last_query();
         return $query->result();
     }
 
